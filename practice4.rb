08cf41e133a50c7f110c300ebc646b7036d0c2c5
oldyama <fuyukinishizawa@gmail.com>
2019-12-16 00:34:51 +0900

    practice5

--- practice4.rb
+++ practice4.rb
@@ -1,4 +1,8 @@
-numbers = [*1..100]
-exclusion_numbers = numbers.reject{|number| number % 3 == 0 || number % 7 == 0}
+#numbers = [*1..100]
+#exclusion_numbers = numbers.reject{|number| number % 3 == 0 || number % 7 == 0}
 
-puts exclusion_numbers 
+#puts exclusion_numbers 
+
+numbers = [*1..100]
+puts numbers.reject{|number| number % 3 == 0 || number % 7 == 0}
+
---
+++ practice5.rb
@@ -0,0 +1,8 @@
+array = [1, "2", "3", 4, 5, 6, "7", 8, "9", 10]
+
+for element in array 
+  if element.is_a?(String)
+    str = element 
+    puts str
+  end
+end
